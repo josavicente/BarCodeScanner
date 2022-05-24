499e5028d71fa4838d5ac7192f41f8d10f3d823c
Josa Vicente <jvicenpe@gmail.com>
2022-05-24 21:32:40 +0200

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Josafat Vicente Pérez on 24/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarCodeScanner
+//
+//  Created by Josafat Vicente Pérez on 24/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
